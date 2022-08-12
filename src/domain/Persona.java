808e1354beb0f7760470326de338fabfa4bdc24b
Samantha González <samujm76@gmail.com>
2022-08-12 13:55:23 -0500

    Todo lo privado no se hereda a las clases hijas

--- src/domain/Persona.java
+++ src/domain/Persona.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package domain;
 
-/**
- *
- * @author Samantha
- */
+
 public class Persona {
+    private String nombre; //Todo lo privado no se hereda a las clases hijas
     
 }
