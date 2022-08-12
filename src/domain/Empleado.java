a3faa94802e826a9220e30e32a990fc4a02aa519
Samantha González <samujm76@gmail.com>
2022-08-12 14:33:36 -0500

    Aunque no se define nada en la clase EMPLEADO podemos acceder a los metodos de PERSONA por extender

--- src/domain/Empleado.java
+++ src/domain/Empleado.java
@@ -2,5 +2,8 @@
 package domain;
 
 public class Empleado extends Persona{
+    //En java solo existe herencia simple
+    //Aunque no se define nada en la clase EMPLEADO podemos acceder a los metodos de PERSONA por extender
+    //LOS CONSTRUCTORES NO SE HEREDAN pero se acceden a traves de la palabra SUPER
     
 }
---
+++ src/test/TestHerencia.java
@@ -0,0 +1,11 @@
+
+package test;
+import domain.Empleado;
+
+public class TestHerencia {
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado();
+        //Aunque no se define nada en la clase EMPLEADO podemos acceder a los metodos de PERSONA por extender
+        System.out.println("empleado1 = " + empleado1);
+    }
+}
