7a4b092fcd13ac5b28780c7059d7a92925ce75a1
Samantha González <samujm76@gmail.com>
2022-08-12 13:57:54 -0500

    Creando los atributos protected para herencia

--- src/domain/Persona.java
+++ src/domain/Persona.java
@@ -3,6 +3,8 @@ package domain;
 
 
 public class Persona {
-    private String nombre; //Todo lo privado no se hereda a las clases hijas
-    
+    protected String nombre; //Todo lo privado no se hereda a las clases hijas, pero protected si
+    protected char genero;
+    protected int edad;
+    protected String direccion;
 }
